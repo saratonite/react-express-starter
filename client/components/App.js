35e554f1aac57d73185c97a55080d5d8a00141df
Sarath <sarath.hacks@gmail.com>
2016-10-30 21:31:17 +0530

    Added App component, updated package.jso with script :smile:

---
+++ client/components/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+export default class App extends React.Component {
+
+  render(){
+    return(
+      <div >
+        <h1>Hello World </h1>
+        <p>;)</p>
+      </div>
+    )
+  }
+}
--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import {render} from 'react-dom';
+import App from './components/App';
 
-ReactDom.render(<h1>Hello World</h1>,document.getElementById('app'));
+render(<App/>,document.getElementById('app'));
